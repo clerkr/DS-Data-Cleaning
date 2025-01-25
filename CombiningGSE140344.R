fc8ca3e9f45e3de38b6c2665e9672f8358e08556
Shu-Fei Lo <shufeilo@byu.edu>
2025-01-24 18:11:52 -0700

    Newest Version of combining Meth/Unmeth columns

--- CombiningGSE140344.R
+++ CombiningGSE140344.R
@@ -1,19 +1,29 @@
 library(tidyverse)
 
+dir_path = "C:/Users/sophi/Documents/Piccolo's Lab/GSE140344/GSE140344_new.tsv"
 file1 = "C:/Users/sophi/Documents/Piccolo's Lab/GSE140344/GSE140344_Meth_UnMethSamples.csv"
 tsv_file = read_tsv(file1)
 
 # Add ID_REF to be the first column name
 colnames(tsv_file) = c("ID_REF", colnames(tsv_file))
 
-# Extract column names
-column_names = colnames(tsv_file)
-methylated_colnames = column_names[which(grepl("Meth", column_names))]
-unmethylated_colnames = column_names[which(grepl("Unmeth", column_names))]
 
-# Extract first 
+# The last column is a little weird (CTF29_Meth)
+# It only contains methylation value in <chr>
+# and it does not have unmethylation value
+# So I'm assuming the white space divides the value into meth and unmeth
+# Under this assumption, I'm gonna separate the column into two columns
+separate(tsv_file, CTF29_Meth, into = c("CTF29_Meth",
+      "CTF29_Unmeth"), sep = "\t",) %>%
+  # Change all columns into <dbl> except for first column
+  mutate(across(-1, as.numeric)) -> modified_data 
+  
 
-#tsv_file |>
-#  head(n = 10) |>
-#  print()
-
+modified_data %>%
+  pivot_longer(cols = -ID_REF, names_to = c("Sample", "Type"),
+               names_pattern = "(.*)_(Meth|Unmeth)", values_to = "Values") %>%
+  mutate(ID_REF = paste(Sample, ID_REF, sep = ".")) %>% # Prefix ID_REF with Sample
+  select(ID_REF, Values, Type) %>%
+  spread(Type, Values) %>%
+  rename(Signal_A = Meth, Signal_B = Unmeth) %>%
+  write_tsv(dir_path)
