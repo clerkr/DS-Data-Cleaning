f1212ad6f2c4e803ad825b729d7168cdfc862f0d
Clark Roylance <j.clark.roylance@gmail.com>
2025-03-06 11:55:17 -0700

    Added scripts for retrieving and preprocessing hg38 and fetching our first sra file

---
+++ BS_processing/human_reference_genome_processing.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define the filename and URL
+FILE="hg38.fa.gz"
+URL="ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
+CONTAINER_IMAGE="quay.io/biocontainers/bismark:0.24.2--hdfd78af_0"
+MOUNT_PATH="$PWD"
+INPUT_FILE="$MOUNT_PATH/hg38.fa"
+
+# Check if the file already exists
+if [ -f "$INPUT_FILE" ]; then
+    echo "$INPUT_FILE already exists. Skipping download and extraction."
+else
+    if [ -f "$FILE" ]; then
+        echo "$FILE already exists. Skipping download."
+    else
+        echo "Downloading $FILE..."
+        wget "$URL"
+        if [ $? -ne 0 ]; then
+            echo "Download failed." >&2
+            exit 1
+        fi
+    fi
+    
+    echo "Extracting $FILE..."
+    gunzip -f "$FILE"
+    if [ $? -eq 0 ]; then
+        echo "Extraction completed successfully."
+    else
+        echo "Extraction failed." >&2
+        exit 1
+    fi
+fi
+
+# Check if the container image is already pulled
+docker image inspect "$CONTAINER_IMAGE" > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Pulling Docker container $CONTAINER_IMAGE..."
+    docker pull "$CONTAINER_IMAGE"
+fi
+
+# Run Bismark genome preparation in the container
+echo "Running Bismark genome preparation..."
+docker run -d -v "$MOUNT_PATH:/home" "$CONTAINER_IMAGE" bismark_genome_preparation "/home" "/home/hg38.fa"
